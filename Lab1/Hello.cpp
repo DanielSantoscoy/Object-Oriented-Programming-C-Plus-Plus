01e56f4bbd8a83636203048799d4582e8bfaad0a
Daniel Santoscoy <danieldaniel356078@gmail.com>
2024-02-07 18:17:16 -0800

    Added Lab1 folder with Hello.cpp file
    Added original Lab1 folder with Hello.cpp file inside Lab1 folder

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,14 @@
+/***
+ * {Lab 1 - Learning how to use Github, CLI, and compile c++ in Visual Studio}
+ ***/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
