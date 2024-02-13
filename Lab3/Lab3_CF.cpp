b942ffa802cdf9108db0013df091ba5782959ccc
Daniel Santoscoy <danieldaniel356078@gmail.com>
2024-02-13 13:25:17 -0800

    Updated Lab3_CF.cpp file
    added lab description

--- Lab3/Lab3_CF.cpp
+++ Lab3/Lab3_CF.cpp
@@ -1,3 +1,16 @@
+// Lab3_CF.cpp
+/**
+ * @file Lab3_CF.cpp
+ * @author Daniel Santoscoy (you@domain.com)
+ * @brief {Combining while and if/else statments}
+ * @version 0.1
+ * @date 2024-02-13
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
 #include<iostream>
 using namespace std;
 
