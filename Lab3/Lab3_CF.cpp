8645dfb7ad18375f6e5ab5a38a4d0f321f1acd29
Daniel Santoscoy <danieldaniel356078@gmail.com>
2024-02-19 13:25:40 -0800

    edited Lab3_CF.cpp file
    added while loop and if/else statements to test control flow diagram

--- Lab3/Lab3_CF.cpp
+++ Lab3/Lab3_CF.cpp
@@ -16,6 +16,22 @@ using namespace std;
 
 int main()
 {
+    int x = 0;
+    int y = 5;
+    
+    while (x < y) {
+        printf("%i\n",x);
+        x = x + 3;
+    }
+
+    if (x > y)
+    {
+        x % 2 == 0;
+    } else if (x % 2 == 0){
+        printf("x is even\n");
+    } else {
+        printf("x is odd\n");
+    }
     
     return 0;
 }
---
+++ Lab3/test.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    
+    int x = 0;
+    int y = 5;
+    
+    while (x < y) {
+        printf("%i\n",x);
+        x++;
+    }
+}
