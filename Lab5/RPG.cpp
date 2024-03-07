606cbaa0fb4bad0d34dbf0ac1069186116ecd4dd
Daniel Santoscoy <danieldaniel356078@gmail.com>
2024-03-07 13:19:41 -0800

    Updated RPG.cpp
    added RPG() function with paramaters, and called functions from RPG.h inside RPG() function and also added void function setSkills() with if/else if, and else statements for characters and moves assigned to characters. also added void printAction() function to print action occuring

--- Lab5/RPG.cpp
+++ Lab5/RPG.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
-#include RPG.h
+#include "RPG.h"
 using namespace std;
 
-int RPG()
+RPG::RPG()
 {
+    //Assign private variables
    string name = "NPC";
    int health = 100;
    int strength = 10;
@@ -11,5 +12,37 @@ int RPG()
    string type = "warrior";
    string skills[0] = "slash";
    string skills[1] = "parry";
-    return 0;
+};
+
+RPG::RPG(string name, int health, int strength, int defense, string type){
+    //Call functions from RPG.h
+    setSkills();
+    getName();
+    getHealth();
+    getStrength();
+    getDefense();
+};
+
+/**
+ * @brief sets the skills based on RPG's role
+ * 
+ */
+void RPG::setSkills() {
+    if (type == "mage"){
+        skills[0] = "fire";
+        skills[1] = "thunder";
+    } else if (type == "thief"){
+        skills[0] = "pilfer";
+        skills[1] = "jab";
+    } else if (type == "archer"){
+        skills[0] = "parry";
+        skills[1] = "crossbow_attack";
+    } else {
+        skills[0] = "slash";
+        skills[1] = "parry";
+    }
+};
+
+void RPG::printAction(string skill, RPG opponent) {
+    printf("%s used %s on %s\n", name.c_str(), opponent.getName().c_str());
 }
