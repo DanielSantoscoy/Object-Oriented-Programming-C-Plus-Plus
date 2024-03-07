bba3d2a22a661325a893b431956116a70b631f28
Daniel Santoscoy <danieldaniel356078@gmail.com>
2024-03-06 16:57:52 -0800

    Updated RPG.h
    added functions in public class and variables in private

--- Lab5/RPG.h
+++ Lab5/RPG.h
@@ -9,6 +9,41 @@ class RPG {
         //constructors
         RPG();
         RPG(string name, int health, int strength, int defense, string type);
+
+        //Complete the rest
+        void setSkills();
+
+        void printAction(string name, RPG);
+
+        void updateHealth(int health);
+
+        void attack(RPG *);
+
+        void useSkill(RPG *);
+
+        const bool isAlive();
+
+        const string getName();
+
+        const int getHealth();
+
+        const int getStrength();
+
+        const int getDefense();
+
+    private:
+        string name = "Wiz";
+        
+        int health = 100;
+       
+        int strength = 100;
+      
+        int defense = 100;
+        
+
+        //Complete the rest
+        string.type; //warrior, mage, thief, archer
+        string skills[SKILL_SIZE];
 };
 
 #endif
