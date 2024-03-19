6574b287151226008cdfd8fef98a83b20fca4fee
Daniel Santoscoy <danieldaniel356078@gmail.com>
2024-03-19 10:33:00 -0700

    updated main.cpp
    changed player 1 and player 2 to just p1 and p2, as well as created variables for players and created functions displayStats, displayEnd, and gameLoop

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -5,20 +5,76 @@ using namespace std;
 int main()
 {
     //Create RPG that uses the default constructor
-    RPG npc;
+    RPG p2;
 
     //Create RPG that uses the overloaded constructor
-    RPG player("Player", 100, 20, 15, "warrior");
+    RPG p1("Wiz", 70, 45, 15, "mage");
 
     // Test isAlive function
-    cout << "Is player alive? " << (player.isAlive() ? "Yes" : "No") << endl;
+    cout << "Is p1 alive? " << (p1.isAlive() ? "Yes" : "No") << endl;
 
     // Test updateHealth function
     int newHealth = 75;
-    player.updateHealth(newHealth);
-    cout << "Updated player health: " << player.getHealth() << endl;
-    cout << "Is player alive now? " << (player.isAlive() ? "Yes" : "No") << endl;
+    p1.updateHealth(newHealth);
+    cout << "Updated p1 health: " << p1.getHealth() << endl;
+    cout << "Is p1 alive now? " << (p1.isAlive() ? "Yes" : "No") << endl;
 
-    
+    // Create variables for players
+    RPG p1 = RPG("Wiz", 70, 45, 15, "mage");
+    RPG p2 = RPG();
+    // implement gameLoop(), displayEnd(), and displayStats()
+    gameLoop(&p1, &p2);
+    displayEnd(p1, p2);
     return 0;
+}
+
+/**
+ * @brief Prints out both player's name and their health.
+ * Example Output for names Wiz and NPC
+ * 
+ * @param player1
+ * @param player2
+ */
+void displayStats(RPG p1, RPG p2) {
+    //Implement the brief
+    printf("Wiz health: " p1->gethealth())
+}
+
+/**
+ * @brief Displays who wins based on who is alive. Use an if statement to check whether
+ * player 1 is alive. If so, print that they won, else player 2 won.
+ * 
+ * Example output:
+ * Wiz defeated NPC! Good game!\n
+ * 
+ * @param player 1
+ * @param player 2
+ */
+void displayEnd(RPG p1, RPG p2) {
+    //Implement brief
+    if (p1.isAlive()) {
+        printf("Wiz defeated NPC! Good game!\n");
+    } else {
+        printf("Player 2 wins! Good game!\n");
+    }
+}
+/***
+* @brief uses a while loop to check whether both players are alive.
+* If so, it calls displayStats((*player1), (*player2))
+* Inside the while loop it:
+* Then prints the name of player one to say that it is their turn
+* Then call (*player1).useSkill(player2)
+Then prints "---------------------------------------\n"
+Repeat the process for player 2 acting on player 1 inside the same while loop
+* Then prints the name of player two to say that it is their turn
+Then call (*player2).useSkill(player1)
+* Then prints
+-\n"
+Refer to the Role Playing Game Part Two instructions for the
+example outut
+* @param player1
+* @param player2
+*/
+void gameLoop(RPG * player1, RPG * player2){
+//Implement brief
 }
