5199ba77f96650df5b7b5875c4d2d07807e27e3f
Daniel Santoscoy <danieldaniel356078@gmail.com>
2024-03-21 13:14:17 -0700

    updated main.cpp
    added function and instructions from Lab 7 assignment

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -1,8 +1,38 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
+int main() {
+    const int SAMPLE_SIZE = 10;
+    int array_int[SAMPLE_SIZE] = {1,2,3,4,5,6,7,8,9,10};
+    string array_str[SAMPLE_SIZE] = {"This", "is", "a", "sentence. ",
+                                    "This", "is", "another", "sentence.",
+                                    "The", "end."};
+    
+    char array_char[SAMPLE_SIZE] = {'a', 'b', 'c', 'd', 'e', '0', '1', '2', '3', '4'};
+
+    string int_file = "array_int.csv";
+    writeToFile(int_file, array_int, SAMPLE_SIZE); // arr is already a pointer
+
+    // Do the same for the other arrays
+
+    // CREATE a string variable as the filename and assign
+    // it the value "array_str.csv"
+
+
+    // CALL writeToFile using the string variable above, array_str, and SAMPLE_SIZE as
+    // parameters
+
+
+    // CREATE a string variable as the filename and assign
+    // it the value "array_str.csv"
+
+
+    // CREATE a string variable as the filename and assign
+    // it the value "array_str.csv"
+
+
+    // CALL readFile() on all 3 generated files.
+
     
     return 0;
 }
