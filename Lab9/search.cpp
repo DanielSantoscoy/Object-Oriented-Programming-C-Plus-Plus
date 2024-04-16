e425a331d030cc6ded92982c7e27d897239fe39b
Daniel Santoscoy <danieldaniel356078@gmail.com>
2024-04-16 13:09:25 -0700

    updated search.cpp
    added iterativeSearch function with param v and elem from instructions given

---
+++ Lab9/search.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<fstream>
+#include<ctime>
+using namespace std;
+
+
+/**
+ * @brief returns the first index of elem if it exists, otherwise returns -1
+ * iterativeSearch starts at the first index and iterates sequentially to the next until it either 
+ * finds the element or until it reaches the end (i.e. element does not exist)
+ * 
+ * @param v : vector of elements
+ * @param elem : integer to look for 
+ * @return int 
+ */
+int iterativeSearch(vector<int> v, int elem) {
+    // use a for loop where the index, i goes from 0 to the size of v
+
+    // inside the for loop, use an if statement to check whether the element at i (e.g. v[i]) equals elem
+    // inside the if statement return -1
+    
+}
+
+int main()
+{
+    
+    return 0;
+}
