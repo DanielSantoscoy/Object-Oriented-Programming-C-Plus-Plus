eb91cc8d444d973912cf7662dc09f700990a9f65
Daniel Santoscoy <danieldaniel356078@gmail.com>
2024-04-16 13:25:50 -0700

    updated search.cpp
    added binarySearch function with instructions given by instructor

--- Lab9/search.cpp
+++ Lab9/search.cpp
@@ -19,6 +19,44 @@ int iterativeSearch(vector<int> v, int elem) {
 
     // inside the for loop, use an if statement to check whether the element at i (e.g. v[i]) equals elem
     // inside the if statement return -1
+
+}
+
+
+/**
+ * @brief returns the index of elem, if it exists in v. Otherwise it returns -1.
+ * binarySearch is recursive (i.e. functions calls itself).
+ * It utilizes the fact that v is increasing order (e.g. values go up and
+ * duplicates are not allowed).
+ * 
+ * It calculates the mid from the start and end index. It compares v[mid] (i.e. value
+ * at mid) with elem and decides whether to search the left half (lowewr values)
+ * or right half (upper values).
+ * 
+ * 
+ * @param v : vector of elements 
+ * @param start : leftmost index (starting value is 0)
+ * @param end : rightmost index (starting value is v.size())
+ * @param elem : integer to look for
+ * @return int 
+ */
+
+int binarySearch(vector<int> & v, int start, int end, int elem) {
+    // write an if statement that checks the terminating case 
+    // inside the statement return -1
+
+    // instantiate the midpoint
+
+
+    // Use if/else statements to do the following:
+    // 1) update end (search left half)
+
+    // 2) update start (search right half)
+
+    // 3) return mid (found the elem)
+
+    // return a recursive call to binarySearch(...)
+
     
 }
 
