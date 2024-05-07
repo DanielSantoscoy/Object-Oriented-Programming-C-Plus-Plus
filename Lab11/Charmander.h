e9eb9a8617f0fde83047f620d6da6abc9aea9c12
Daniel Santoscoy <danieldaniel356078@gmail.com>
2024-05-07 11:43:21 -0700

    updated Charmander.h
    fixed type in Charmander, previously had it as "Charmender"

--- Lab11/Charmander.h
+++ Lab11/Charmander.h
@@ -1,16 +1,16 @@
-#ifndef CHARMENDER_H
-#define CHARMENDER_H
+#ifndef CHARMANDER_H
+#define CHARMANDER_H
 
 #include<string>
 #include<vector>
 #include<Pokemon.h>
 using namespace std;
 
-class Charmender : public Pokemon {
+class Charmander : public Pokemon {
     public:
         //---------------- Constructors ---------------
-        Charmender();
-        Charmender(string name, int hp, int attack, int defense, vector<string> t, vector<string> s);
+        Charmander();
+        Charmander(string name, int hp, int att, int def, vector<string> t, vector<string> s);
         //---------------- Mutator Functions -----------------
         void speak()/* override */;
         void printStats()/*override*/;
