d242b48584634765735b54b7924a176d87807fae
Daniel Santoscoy <danieldaniel356078@gmail.com>
2024-05-07 11:03:12 -0700

    updated Charmander.h
    added starter code given by professor

--- Lab11/Charmander.h
+++ Lab11/Charmander.h
@@ -1,3 +1,21 @@
+#ifndef CHARMENDER_H
+#define CHARMENDER_H
+
 #include<string>
 #include<vector>
 #include<Pokemon.h>
+using namespace std;
+
+class Charmender : public Pokemon {
+    public:
+        //---------------- Constructors ---------------
+        Charmender();
+        Charmender(string name, int hp, int attack, int defense, vector<string> t, vector<string> s);
+        //---------------- Mutator Functions -----------------
+        void speak()/* override */;
+        void printStats()/*override*/;
+        private:
+            vector<string> skills;
+            /* name , hp, attack, defense */
+};
+#endif
